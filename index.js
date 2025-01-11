06020e80e8033ce1fcfbb3011054d5abaaf7cf05
Michael <69610401+michaelsiweb@users.noreply.github.com>
2025-01-10 21:41:34 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+ 
+// Endpoint da API
+app.get('/api/gatilho', (req, res) => {
+    res.send('permitido'); // Altere para "bloqueado" quando necessário
+});
+ 
+// Configura a porta
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`API rodando na porta ${port}`);
+});
